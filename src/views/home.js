57dea824205290c2ffb5de2270807acbd23e71d9
Nicolas Fritz <n1xx0n@Nicolas-Fritzs-MacBook-Pro.local>
2022-06-06 20:52:21 +0200

    added itinery

--- src/views/home.js
+++ src/views/home.js
@@ -3,7 +3,7 @@ import img from "../images/cruiseship.jpg"
 import { useSearchParams } from 'react-router-dom';
 
 function DisplayError() {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   var error = searchParams.get("error");
   var errormsg = searchParams.get("error_description");
   if (error != null) {
@@ -14,15 +14,23 @@ function DisplayError() {
 }
 
 function CruiseShipImage(){
-    return <img src={img} alt="Cruise1, Ahoi!"/>
+    return <img src={img} alt="Cruise1, Ahoi!" className="photo"/>
    }
   
 const Home = () => (
   <Fragment>
     {CruiseShipImage()}      
-    <p>
-        Cruise1
-    </p>
+    <h1>Cruise1</h1>
+      <div className="iternary">
+        <h2>Iternary</h2>
+        <ol>
+          <li><b>Introduction</b> - The management console and the connection to a SPA.</li>
+          <li><b>Universal Login</b> - How the new login experience can be customized.</li>
+          <li><b>Social login</b> - Enable social identity providers for your app.</li>
+          <li><b>Email verification</b> - Multiple ways to ensure verified emails.</li>
+          <li><b>Account linking</b> - Avoid duplicate accounts with social login.</li>
+        </ol>
+    </div>
     {DisplayError()}
   </Fragment>
 );
